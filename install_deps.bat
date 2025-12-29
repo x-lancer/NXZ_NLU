04fc30a14caf7bb6a8a1988ad5d48dbe487c7694
ym <lancer@txzxing.com>
2025-12-29 11:43:03 +0800

    feat: update deploy script

--- install_deps.bat
+++ install_deps.bat
@@ -3,10 +3,14 @@ echo ==========================================
 echo       NXZ NLU Service - Environment Setup
 echo ==========================================
 
-echo [1/2] Upgrading pip...
+echo [1/3] Upgrading pip...
 python -m pip install --upgrade pip
 
-echo [2/2] Installing requirements...
+echo [2/3] Installing PyTorch (CPU Version)...
+echo Note: Installing CPU version to save space and avoid CUDA errors.
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+
+echo [3/3] Installing other requirements...
 pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 
 echo.
@@ -14,4 +18,3 @@ echo ==========================================
 echo       Installation Complete!
 echo ==========================================
 pause
-
